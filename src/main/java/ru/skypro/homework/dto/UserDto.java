d68c5012980f8d24a87b8d8220dfdf5cfe90cb0e
Amilenkof <f.o.g@list.ru>
2023-11-19 16:33:37 +0400

    week 1

---
+++ src/main/java/ru/skypro/homework/dto/UserDto.java
@@ -0,0 +1,17 @@
+package ru.skypro.homework.dto;
+
+import lombok.Data;
+import ru.skypro.homework.model.Role;
+
+@Data
+public class UserDto {
+
+    private long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private Role role;
+    private String image;
+
+}
